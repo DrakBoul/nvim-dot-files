bbc5918a8872de9824ce565ba7447ba836ee78a1
DrakBoul <dboul63@gmail.com>
2025-10-06 12:17:37 -0600

    added plugins and some keybind

--- init.lua
+++ init.lua
@@ -23,60 +23,63 @@ vim.opt.signcolumn = "yes"
 vim.cmd("set winborder=rounded")
 
 -- LSP
-vim.lsp.enable({ "lua_ls", "rust_analyzer", "clangd" })
+vim.lsp.enable({ "lua_ls", "rust_analyzer", "clangd", "jdtls" })
 
 -- Install Plugins
 vim.pack.add({
     { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/stevearc/oil.nvim.git" },
-    { src = "https://github.com/nvim-mini/mini.pick.git" },
+    { src = "https://github.com/nvim-telescope/telescope.nvim.git" },
     { src = "https://github.com/neovim/nvim-lspconfig.git" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter.git" },
+    { src = "https://github.com/mason-org/mason.nvim.git" },
+    { src = "https://github.com/nvim-lua/plenary.nvim.git" },
 })
 
 -- Setup Plugins
-require("mini.pick").setup()
+require('mason').setup()
+require('telescope').setup({})
 require("oil").setup()
 require("nvim-treesitter.configs").setup {
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust" },
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = false,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = false,
 
-  -- List of parsers to ignore installing (or "all")
-  ignore_install = { "javascript" },
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = { "javascript" },
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = {},
-    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-    disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-    end,
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        disable = {},
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
 
 -- Colorscheme
@@ -86,8 +89,49 @@ vim.cmd("colorscheme vague")
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>d', '<cmd>Oil<CR>')
-vim.keymap.set('n', '<leader>sf', '<cmd>Pick files<CR>')
-vim.keymap.set('n', '<leader>sb', '<cmd>Pick buffers<CR>')
-vim.keymap.set('n', '<leader>sg', '<cmd>Pick grep<CR>')
+vim.keymap.set('n', '<leader>sf', '<cmd>Telescope find_files<CR>')
+vim.keymap.set('n', '<leader>sb', '<cmd>Telescope buffers<CR>')
+vim.keymap.set('n', '<leader>sg', '<cmd>Telescope live_grep<CR>')
+vim.keymap.set('n', '<leader>sh', '<cmd>Telescope help_tags<CR>')
+vim.keymap.set('n', '<ESC><ESC>', '<cmd>nohlsearch<CR>')
 
+
+-- Commands
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking (copying) text',
+    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
+
+-- Diagnostics
+vim.diagnostic.config({
+    virtual_text = true,
+    signs = true,
+    underline = true,
+})
+
+
+-- Cursor settings
+vim.cmd("set guicursor=a:block")
+
+-- Autocompletion settings
+vim.cmd("set completeopt=fuzzy,menu,menuone,noinsert")
+-- Make it so enter does not accept completion
+vim.cmd("inoremap <expr> <CR> pumvisible() ? \"<C-e><CR>\" : \"<CR>\"")
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('my.lsp', {}),
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        if client:supports_method('textDocument/completion') then
+
+            local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+            client.server_capabilities.completionProvider.triggerCharacters = chars
+            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+
+        end
+    end,
+})
 
