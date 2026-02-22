b5d088ba46d5ed500243ab345ee88b0ea7dc930e
Drake Boulianne <dboul63@gmail.com>
2026-02-21 19:47:42 -0800

    colorscheme, find command upgrade, removed telescope, option configuration

--- init.lua
+++ init.lua
@@ -1,48 +1,49 @@
--- Options
+-- -- Options
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
-vim.opt.relativenumber = true
-vim.opt.number = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.o.signcolumn = 'yes'
 vim.o.inccommand = 'split'
+vim.o.incsearch = true
 vim.o.cursorline = true
 vim.o.showmode = true
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.mouse = ""                  -- no mouse
+vim.o.guicursor = "a:block"
+vim.o.grepprg = "rg --vimgrep -u" -- stop rg from searching hidden directories
+vim.o.scrolloff = 30
+vim.o.scroll = 10
+vim.o.pumheight = 10
+vim.o.pumborder = "rounded"
+vim.o.wildmode = "noselect:lastused,full"
+vim.o.wildoptions = "fuzzy"
+vim.o.wildmenu = true
+vim.o.completeopt = "fuzzy,menu,menuone,noinsert"
 
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.expandtab = true
+vim.lsp.enable({ "lua_ls", "rust_analyzer", "clangd", "pyright" })
 
-vim.opt.scrolloff = 30
-vim.opt.scroll = 10
-
-vim.opt.signcolumn = "yes"
-vim.cmd("set winborder=rounded")
-
--- LSP
-vim.lsp.enable({ "lua_ls", "rust_analyzer", "clangd", "jdtls" })
-
--- Install Plugins
+-- -- Install plugins
 vim.pack.add({
     { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/stevearc/oil.nvim.git" },
-    { src = "https://github.com/nvim-telescope/telescope.nvim.git" },
     { src = "https://github.com/neovim/nvim-lspconfig.git" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter.git" },
     { src = "https://github.com/mason-org/mason.nvim.git" },
     { src = "https://github.com/nvim-lua/plenary.nvim.git" },
+    { src = "https://github.com/sainnhe/everforest.git" },
 })
 
--- Setup Plugins
+-- Setup plugins
 require('mason').setup()
-require('telescope').setup({})
 require("oil").setup()
 require("nvim-treesitter.configs").setup {
     -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust" },
+    ensure_installed = { "c", "python", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
@@ -73,7 +74,6 @@ require("nvim-treesitter.configs").setup {
                 return true
             end
         end,
-
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -82,19 +82,24 @@ require("nvim-treesitter.configs").setup {
     },
 }
 
--- Colorscheme
-vim.cmd("colorscheme vague")
+-- -- Colorscheme
+vim.cmd("colorscheme everforest")
 
--- Keybinds
+-- -- Keybinds
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>d', '<cmd>Oil<CR>')
-vim.keymap.set('n', '<leader>sf', '<cmd>Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>sb', '<cmd>Telescope buffers<CR>')
-vim.keymap.set('n', '<leader>sg', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>sh', '<cmd>Telescope help_tags<CR>')
 vim.keymap.set('n', '<ESC><ESC>', '<cmd>nohlsearch<CR>')
-
+-- Quick fix
+vim.keymap.set('n', '<leader>q', '<cmd>copen<CR>')
+vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>')
+vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>')
+-- Tab pages
+vim.keymap.set('n', '<leader>c', '<cmd>tabnew<CR>')
+vim.keymap.set('n', '<leader>x', '<cmd>tabclose<CR>')
+vim.keymap.set('n', '<leader>n', '<cmd>tabnext<CR>')
+vim.keymap.set('n', '<leader>p', '<cmd>tabprev<CR>')
+vim.keymap.set("n", "<leader>s", ":find ")
 
 -- Commands
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -105,19 +110,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
--- Diagnostics
+-- -- Diagnostics
 vim.diagnostic.config({
     virtual_text = true,
     signs = true,
     underline = true,
 })
 
+-- -- Insert autocompletion
 
--- Cursor settings
-vim.cmd("set guicursor=a:block")
-
--- Autocompletion settings
-vim.cmd("set completeopt=fuzzy,menu,menuone,noinsert")
 -- Make it so enter does not accept completion
 vim.cmd("inoremap <expr> <CR> pumvisible() ? \"<C-e><CR>\" : \"<CR>\"")
 
@@ -126,12 +127,27 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
         local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
         if client:supports_method('textDocument/completion') then
-
             local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
             client.server_capabilities.completionProvider.triggerCharacters = chars
             vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-
         end
     end,
 })
 
+-- -- Command line autocompletion
+vim.cmd("autocmd CmdlineChanged [:\\/\\?] call wildtrigger()")
+
+-- Remap left and right keys so they don't cycle completions in wildmenu
+vim.cmd("cnoremap <expr> <Left>   wildmenumode() ? \"<C-E><Left>\"   : \"<Left>\"")
+vim.cmd("cnoremap <expr> <Right> wildmenumode() ? \"<C-E><Right>\" : \"<Right>\"")
+
+-- make keybind that mimics '*' but instead pumps it directly into grep and opens quickfix
+
+-- -- Misc
+
+-- Interactive find command with fuzzy finding
+function _G.RgFindFiles(cmdarg, _cmdcomplete)
+    local fnames = vim.fn.systemlist('rg --files --hidden --color=never --glob="!.git"')
+    return #cmdarg == 0 and fnames or vim.fn.matchfuzzy(fnames, cmdarg)
+end
+vim.o.findfunc = 'v:lua.RgFindFiles'
